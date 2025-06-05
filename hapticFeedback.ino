2d379149a8351bc05d23184616520ae3ec7f2f4f
frivolous-impulsor <ultrapilot95@gmail.com>
2025-06-05 15:50:22 -0400

    gyro and accelerometer readings

--- hapticFeedback.ino
+++ hapticFeedback.ino
@@ -1,131 +1,154 @@
-// Basic demo for accelerometer readings from Adafruit MPU6050
-
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
 #include <Wire.h>
-#include <vector>
 
-double Vx = 0;
-double Px = 0;
-double increment;
-#define time_interval 500;
+float rateRoll, ratePitch, rateYaw;
+float rateRollCali, ratePitchCali, rateYawCali;
 
-std::vector<double> acc {0.0,0.0,0.0}; 
+const int rateCaliIterations = 2000;
+const int accCaliIterations = 2000;
 
-Adafruit_MPU6050 mpu;
+
+float accX, accY, accZ;
+float accX_Cali {0} 
+float accY_Cali {0} 
+float accZ_Cali {0};
+float angleRoll, anglePitch, angleYaw;
+
+float loopTimer;
+
+void acc_signals(void){
+  Wire.beginTransmission(0x68); //start I2C communication with gyro
+
+  //Low pass filter to filter out motor vibration noise
+  Wire.write(0x1A);
+  Wire.write(0x05);
+  Wire.endTransmission();
+
+  //Configure the accelerometer output
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1C);
+  Wire.write(0x10);
+  Wire.endTransmission();
+
+  //Read from acceleromter, sensitivity to 4096 LSB/g
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3B); //register of reading starting point
+  Wire.endTransmission();
+  Wire.requestFrom(0x68, 6);
+  int16_t accX_LSB = Wire.read() <<8 | Wire.read();
+  int16_t accY_LSB = Wire.read() <<8 | Wire.read();
+  int16_t accZ_LSB = Wire.read() <<8 | Wire.read();
+
+  accX = (float)accX_LSB/4096;
+  accY = (float)accY_LSB/4096;
+  accZ = (float)accZ_LSB/4096;
+
+  //angleRoll = atan(accY/sqrt(accX*accX + accZ*accZ))*180/3.142;
+  //anglePitch = atan(-accX/sqrt(accY*accY + accZ*accZ))*180/3.142;
+  //anglePitch1 = atan(accZ/sqrt(accX*accX + accY*accY))*180/3.142;
+}
+
+void acc_cali(iterations){
+  for(int i = 0; i<iterations; ++i){
+    acc_signals();
+    accX_Cali += accX;
+    accY_Cali += accY;
+    accZ_Cali += accZ;
+  }
+  accX_Cali /=iterations;
+  accY_Cali /=iterations;
+  accZ_Cali /=iterations;
+}
+
+
+
+//Read gyro signals
+void gyro_signals(void){
+  Wire.beginTransmission(0x68); //start I2C communication with gyro
+
+  //Low pass filter to filter out motor vibration noise
+  Wire.write(0x1A);
+  Wire.write(0x05);
+  Wire.endTransmission();
+
+  //Set the sensitivity scale factor
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1B);
+  Wire.write(0x8);  //choose 65.5LSB/degree/s: reading can be as granular as 65.5 counts per degree/second
+  Wire.endTransmission();
+
+  //Access registers storing gyro measurements
+  Wire.beginTransmission(0x68);
+  Wire.write(0x43); //first register to use for gyro readings
+  Wire.endTransmission();
+  Wire.requestFrom(0x68, 6);  //one reading per axis, two registers per reading, in total 6 requesting registers
+
+  //two registers(in LSB) per reading, store in one 2-byte var
+  int16_t gyroX = Wire.read()<<8 | Wire.read(); 
+  int16_t gyroY = Wire.read()<<8 | Wire.read(); 
+  int16_t gyroZ = Wire.read()<<8 | Wire.read(); 
+
+  //retrieve degree from LSB readings
+  rateRoll = (float)gyroX/65.5; 
+  ratePitch = (float)gyroY/65.5; 
+  rateYaw = (float)gyroZ/65.5; 
+}
 
 void setup(void) {
-  Serial.begin(115200);
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+  Serial.begin(57600);
+  
+  //Set the clock speed of I2C
+  Wire.setClock(400000);  //MPU6050 supports I2C communications at up to 400kHz
+  Wire.begin();
+  delay(250); //give MPU6050 time to start
 
-  Serial.println("Adafruit MPU6050 test!");
+  //Start the gyro in power mode
+  Wire.beginTransmission(0x68);
+  Wire.write(0x6B);
+  Wire.write(0X00);
+  Wire.endTransmission();
 
-  // Try to initialize!
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
+  /*
+  for(int i = 0; i < rateCaliIterations; ++i){
+    gyro_signals();
+    rateRollCali += rateRoll;
+    ratePitchCali += ratePitch;
+    rateYawCali += rateYaw;
+    delay(1);
   }
-  Serial.println("MPU6050 Found!");
+  rateRollCali /= rateCaliIterations;
+  ratePitchCali /= rateCaliIterations;
+  rateYawCali /= rateCaliIterations;
+  */
 
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
+  for(int i = 0; i < accCaliIterations; ++i){
+    acc_signals
   }
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
-
-  Serial.println("");
-  delay(100);
 }
 
 void loop() {
+  acc_signals();
+  /*
+  rateRoll -= rateRollCali;
+  ratePitch -= ratePitchCali;
+  rateYaw -= rateYawCali;
+  //print results in degree/s
+  Serial.print("Roll rate [degree/s] = ");
+  Serial.print(rateRoll);
+  Serial.print("Pitch rate [degree/s] = ");
+  Serial.print(ratePitch);
+  Serial.print("Yaw rate [degree/s] = ");
+  Serial.print(rateYaw);
+  Serial.println();
+  */
 
-  /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-  acc[0] = a.acceleration.x;
-  acc[1] = a.acceleration.y;
-  acc[2] = a.acceleration.z;
-  
-  increment =  acc[0]*time_interval;
-  Vx = Vx + increment;
-  Px = Vx*time_interval;
+  Serial.print(" Acceleration X [g] = ");
+  Serial.print(accX);
 
-  /* Print out the values */
-  //Serial.print("Position X: ");
-  //Serial.println(Px);
+  Serial.print(" Acceleration Y [g] = ");
+  Serial.print(accY);
 
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
-
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
-
-  Serial.println("");
-  delay(500);
+  Serial.print(" Acceleration Z [g] = ");
+  Serial.print(accZ);
+  Serial.println();
+  delay(50);
 }
